14462e31e3b9a6ae6a17ecb19eae1f8de9acc444
Gautam Mallick <gomzi007@gmail.com>
2024-08-25 13:53:28 +0530

    updated with print

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -32,24 +32,33 @@ scissors = '''
 ---.__(___)
 '''
 
-game_choice = [rock, paper, scissors]
-computer_choice = random.randint(0,2)
-user_choice = int(input("Choose 0- rock, 1 - paper, 2 - scissors:\n"))
+game_images = [rock, paper, scissors]
+
+user_choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n"))
+print(game_images[user_choice])
+
+computer_choice = random.randint(0, 2)
+print("Computer chose:")
+print(game_images[computer_choice])
 
 if user_choice >= 3 or user_choice < 0:
     print("You typed an invalid number. You lose!")
-#User - Rock (0) beats Scissors (2)
 elif user_choice == 0 and computer_choice == 2:
     print("You win!")
+elif computer_choice == 0 and user_choice == 2:
+    print("You lose!")
 # The following condition checks which choice is 'greater'
 # Rock (0) < Paper (1) < Scissors (2) in this context
 # So, if the computer's choice is greater than the user's choice,
 # the computer wins.
-elif computer_choice == 0 and user_choice == 2:
-    print("You lose!")
 elif computer_choice > user_choice:
     print("You lose!")
 elif user_choice > computer_choice:
     print("You win!")
+elif computer_choice == user_choice:
+    print("It's a draw!")
+
+
+
 elif computer_choice == user_choice:
     print("It's a draw!")
